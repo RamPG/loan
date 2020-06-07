72b60559efb9902921c374a0d2037b951b3ec1a2
git config --global user.name <vlasovnew@yandex.ru>
2020-06-07 11:49:56 +0300

    add slider styles

--- js/modules/Page.js
+++
@@ -1,6 +0,0 @@
-export default class Page {
-    constructor({pageSelector = null}) {
-        this.page = document.querySelector(pageSelector);
-        this.pageBlocks = this.page.children;
-    }
-}
